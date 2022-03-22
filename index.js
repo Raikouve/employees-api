f107176f2c923ac71a9654fddc1cce2ac7ddfd33
Antônio Gonçalves <tonygoldmarket@yahoo.com>
2022-03-22 14:56:02 -0300

    added error middleware on handlers

--- index.js
+++ index.js
@@ -6,28 +6,28 @@ const getEmployeeByName = require("./handlers/getEmployeeByName");
 const createEmployee = require("./handlers/createEmployee");
 const updateEmployee = require("./handlers/updateEmployee");
 const deleteEmployee = require("./handlers/deleteEmployee");
+const { error } = require("./middlewares");
 
 const HTTP_STATUS_OK = 200;
 const PORT = 3000;
 
 const app = express();
 
-
 app.use(bodyParser.json());
 
 app.get("/", (_req, res) => res.status(HTTP_STATUS_OK).send());
 
-app.get("/employees/search", (req, res, next) => getEmployeeByName(req, res, next));
+app.get("/employees/search", error, getEmployeeByName);
 
-app.get("/employees", (_req, res, next) => getAllEmployees(_req, res, next));
+app.get("/employees", error, getAllEmployees);
 
-app.get("/employees/:id", (req, res, next) => getEmployeeById(req, res, next));
+app.get("/employees/:id", error, getEmployeeById);
 
-app.post("/employees", (req, res, next) => createEmployee(req, res, next));
+app.post("/employees", error, createEmployee);
 
-app.put("/employees/:id", (req, res, next) => updateEmployee(req, res, next));
+app.put("/employees/:id", error, updateEmployee);
 
-app.delete("/employees/:id", (req, res, next) => deleteEmployee(req, res, next));
+app.delete("/employees/:id", error, deleteEmployee);
 
 app.listen(PORT, () => {
   console.log('Server On!');
